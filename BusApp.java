2e0ee26b4dbb9aac43290e08a139adee370fc4c5
Ailbhe McEvoy <69002141+AilbheMc@users.noreply.github.com>
2021-04-29 20:57:46 +0100

    Update BusApp.java

--- BusApp.java
+++ BusApp.java
@@ -6,102 +6,171 @@ public class BusApp {
 	static EdgeWeightedDigraph graph;
 	static Dijkstra dijkstra;
 	static TST tree;
-	public static void main(String[] args)
+
+
+	public static void main(String[] args) throws FileNotFoundException
 	{
 		graph = new EdgeWeightedDigraph();
 		tree = new TST();
 		tree.insert();
 		MenuScreen();
-		
-		
-		
 	}
 	
 	private static void MenuScreen()
 	{
-		System.out.println("Welcome to the Vancouver bus app! Please type: \n 1 - To find the shortest path between two bus stops \n 2 - To search for a bus stop by name \n 3 - To search for all trips with a given arrival time \n 4 - To quit the application");
-		String input;
+		
+		System.out.println("Welcome to the Vancouver bus app! ");
+		boolean quitLiterallyEverything = false;
+		while(!quitLiterallyEverything)
+		{
+		System.out.println("Please type: \n 1 - To find the shortest path between two bus stops \n 2 - To search for a bus stop by name \n 3 - To search for all trips with a given arrival time \n 'quit' - To quit the application");
+		String input = "";
 		boolean chosen;
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\\n|\\r|,");
+	
+		
 		do 
-		{
-			input = sc.next();
-			try 
-			{
-				Integer.parseInt(input);
-				if(Integer.parseInt(input)>4)
-				{
-					System.out.println("Please input a smaller number!"); 
-					chosen = true;
-				}
-				else				
-				{
-					chosen = false;
-				}
-				
-			}
-			catch(NumberFormatException e)
-			{
-				System.out.println("Please only enter an integer value, 1, 2, 3 or 4");
-				chosen = true;
-			}
+		{	
+			
+			if((sc.hasNext("1"))|| (sc.hasNext("2"))||(sc.hasNext("3"))|| (sc.hasNext("quit")))
+					{
+						chosen =false;
+						input=sc.next();
+	
+					}
+							
+			else {
+						System.out.println("Please only enter an integer value, 1, 2, 3 or 'quit'");
+						chosen = true;
+			
+				 }
+			
+			
 		}while(chosen);
 		
 		switch(input) 
 		{
-		case "1":
-			System.out.println("You selected the shortest path finder" );
-			System.out.println("Please enter your starting and ending bus stop ids, separated by a comma (,)");
-			try {
-				int firstId = sc.nextInt();
-				int secondId = sc.nextInt();
-				int firstStopId = graph.findVertexById(firstId);
-				int secondStopId = graph.findVertexById(secondId);
-				dijkstra = new Dijkstra(graph, firstStopId);
-				double distance = dijkstra.getDistTo(secondStopId);
-				System.out.println(String.format("Shortest path between stop id %d and stop id %d is %f", firstId, secondId, distance));
-				break;
-			}
-			catch (InputMismatchException e){
-				System.out.println("The input you gave is not valid.");
-				break;
-			}
-			catch(NullPointerException e){
-				System.out.println("One of the Ids you entered does not exist as a valid bus stop. Please try again");
-				break;
-			}
-		case "2":
+			case "1":
+			boolean quit = false;
+			while(!quit) 
+			{
+		
+				System.out.println("You selected the shortest path finder" );
+				System.out.println("Please enter your starting and ending bus stop ids, separated by a comma (,). ");
+				System.out.println("Otherwise type 'quit' to quit or type 'back' to return to the menu. ");
+			
+				try {
+					if(sc.hasNext("back"))
+					{
+						quit = true;
+						break;
+					}
+					
+					else if(sc.hasNext("quit"))
+					{
+						quitLiterallyEverything=true;
+						break;
+						
+					}
+					
+					
+					else
+					{
+						int firstId = sc.nextInt();
+						int secondId = sc.nextInt();
+						int firstStopId = graph.findVertexById(firstId);
+						int secondStopId = graph.findVertexById(secondId);
+						dijkstra = new Dijkstra(graph, firstStopId);
+						double distance = dijkstra.getDistTo(secondStopId);
+						System.out.println(String.format("Shortest path between stop id %d and stop id %d is %f", firstId, secondId, distance));
+					}
+				}
+				catch (InputMismatchException e)
+				{
+					System.out.println("The input you gave is not valid.");
+				}
+			
+				catch(NullPointerException e)
+				{
+					System.out.println("One of the Ids you entered does not exist as a valid bus stop. Please try again");
+				}
+				
+		   }
+			break;
+
+		
+			case "2":
+				boolean quit2 = false;
+				while(!quit2) 
+				{
 			System.out.println("You selected the bus stop finder" );
 			System.out.println("Please enter a bus stop by name" );
+			System.out.println("Otherwise type 'quit' to quit or type 'back' to return to the menu. ");
+			
+			
 			try{
-				String stopName = sc.next();
+				if(sc.hasNext("back"))
+				{
+					quit = true;
+					break;
+				}
+				
+				else if(sc.hasNext("quit"))
+				{
+					quitLiterallyEverything=true;
+					break;
+					
+				}
+				
+				String stopName = sc.next().toUpperCase();
+				
 				System.out.println(tree.search(stopName));
-				break;
 			}
 			catch(InputMismatchException e){
 				System.out.println("Input is invalid. Please try again.");
-				break;
 			}
+				}
+				break;
 		case "3":
+			boolean quit3 = false;
+			while(!quit3) 
+			{
 			System.out.println("You selected the trip finder" );
+			System.out.println("Please enter your desired arrival in hh:mm:ss format." );
+			System.out.println("Otherwise type 'quit' to quit or type 'back' to return to the menu. ");
 			try {
+				
+				if(sc.hasNext("back"))
+				{
+					quit = true;
+					break;
+				}
+				
+				else if(sc.hasNext("quit"))
+				{
+					quitLiterallyEverything=true;
+					break;
+					
+				}
+				
+				
 				String stopTime = sc.next();
 				SearchTrips.printTrips(stopTime);
 			} catch (InputMismatchException e){
 				System.out.println("Input is invalid.");
 			}
+			}
 			break;
-		case "4":
+		case "quit":
 			System.out.println("Goodbye..." );
+			quitLiterallyEverything=true;
 			break;
 		default:
 			System.out.println("Error");
 			break;
 		}
+		}
 	}
-	
-	
-	
 	
 }
